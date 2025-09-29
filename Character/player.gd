60eb6f11041a334a35ab1b7b335bcebf4d63254c
Vinicius Bueno <viniciusbuenodeoliveira2017@gmail.com>
2025-09-29 20:35:10 -0300

    feat(sprites): Ajusta animação do personagem principal

--- Character/player.gd
+++ Character/player.gd
@@ -1,6 +1,5 @@
 extends CharacterBody2D
 
-
 @export var speed : float = 100.0
 @export var jump_velocity : float = -150.0
 @export var double_jump_velocity : float = -100
@@ -11,16 +10,20 @@ var has_double_jumped : bool = false
 var animation_locked : bool = false
 var direction : Vector2 = Vector2.ZERO
 var was_in_air : bool = false
-var original_scale: Vector2 # <-- NOVA VARIÁVEL
+
+# Escalas diferentes
+var idle_scale: Vector2 = Vector2(0.025, 0.025)
+var run_scale: Vector2 = Vector2(0.035, 0.035)
+var jump_scale: Vector2 = Vector2(0.045, 0.045)
 
 
 func _ready() -> void:
-	# Salva a escala inicial do sprite quando o jogo começa
-	original_scale = animated_sprite.scale
+	# Escala inicial = idle
+	animated_sprite.scale = idle_scale
 
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	# Gravidade
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 		was_in_air = true
@@ -32,7 +35,7 @@ func _physics_process(delta: float) -> void:
 			
 		was_in_air = false
 
-	# Handle jump.
+	# Pulo
 	if Input.is_action_just_pressed("jump"):
 		if is_on_floor():
 			jump()
@@ -40,7 +43,7 @@ func _physics_process(delta: float) -> void:
 			velocity.y = double_jump_velocity
 			has_double_jumped = true
 
-	# Get the input direction and handle the movement/deceleration.
+	# Movimento horizontal
 	direction = Input.get_vector("left", "right", "up", "down")
 	if direction:
 		velocity.x = direction.x * speed
@@ -50,7 +53,6 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 	
 	update_animation()
-	
 	update_facing_direction()
 
 
@@ -58,8 +60,10 @@ func update_animation():
 	if not animation_locked:
 		if direction.x != 0:
 			animated_sprite.play("run")
+			animated_sprite.scale = run_scale
 		else:
 			animated_sprite.play("idle")
+			animated_sprite.scale = idle_scale
 
 
 func update_facing_direction():
@@ -72,11 +76,10 @@ func update_facing_direction():
 func jump():
 	velocity.y = jump_velocity
 	animated_sprite.play("jump")
-	# Aumenta a escala para a animação de pulod
-	animated_sprite.scale = Vector2(0.0065, 0.0065) 
+	animated_sprite.scale = jump_scale
 	animation_locked = true
 
-	
+
 func land():
 	animation_locked = false
-	animated_sprite.scale = original_scale 
+	animated_sprite.scale = idle_scale
