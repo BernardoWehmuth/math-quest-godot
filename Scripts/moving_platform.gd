1b4b5707c3d831989d7a2c3abcf59226d8d935fb
Bernardo Wehmuth <bernardowehmuth1109@gmail.com>
2025-11-05 13:17:48 -0300

    consertando plataforma movel

--- Scripts/moving_platform.gd
+++ Scripts/moving_platform.gd
@@ -25,3 +25,4 @@ func move_platform():
 	
 	var platform_tween = create_tween().set_loops().set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_IN_OUT)
 	platform_tween.tween_property(self, "follow", move_direction, duration).set_delay(WAIT_DURATION)
+	platform_tween.tween_property(self, "follow", Vector2.ZERO, duration).set_delay(WAIT_DURATION + 1.5)
